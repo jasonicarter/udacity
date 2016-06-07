2e9cc14717834dbf345209bb302306a910028dc6
Jason Carter <jason.ian.carter@gmail.com>
2016-06-06 20:04:58 -0400

    Added data exploration work

--- P5_Enron_Email_Data_Machine_Learning/final_project/data_exploration.py
+++ P5_Enron_Email_Data_Machine_Learning/final_project/data_exploration.py
@@ -14,24 +14,106 @@
 #
 # POI label: [‘poi’] (boolean, represented as integer)
 
+import sys
+sys.path.append("../tools/")
+from feature_format import featureFormat
+
+
+def scatter_plot(data_dict, x_feature, y_feature):
+    features = ['poi', x_feature, y_feature]
+    data = featureFormat(data_dict, features)
+
+    import matplotlib.pyplot as plt
+    for point in data:
+        x = point[1]
+        y = point[2]
+        if point[0]:
+            plt.scatter(x, y, color="r", marker="*")
+        else:
+            plt.scatter(x, y, color='b', marker=".")
+    plt.xlabel(x_feature)
+    plt.ylabel(y_feature)
+    plt.show()
+
+def fill_nan_values(data_dict):
+    # Update NaN values with 0 except for email address
+    people_keys = data_dict.keys()
+    feature_keys = data_dict[people_keys[0]]
+    for person in people_keys:
+        for feature in feature_keys:
+            if feature != 'email_address' and \
+                data_dict[person][feature] == 'NaN':
+                data_dict[person][feature] = 0
+
+# TODO: break up function
 def exploreData(data_dict):
-    # TODO: data exploration, scatterplots, boxplots, NaN, etc
-
-    # How many data points (people) are in the dataset?
-    # For each person, how many features are available?
-    # How many POIs are there in the E+F dataset? (count where data[person_name]["poi"]==1 )
-
+    print '### DATA EXPLORATION ###'
     people_keys = data_dict.keys()
     feature_keys = data_dict[people_keys[0]]
 
     poi_count = 0
+    poi_keys = []
     for person in people_keys:
-        poi_count += data_dict[person]["poi"]
+        if data_dict[person]["poi"]:
+            poi_count += 1
+            poi_keys.append(person)
 
-    print 'Number of people in dataset: ', len(people_keys)
-    print 'Number of features for each person: ', len(feature_keys)
-    print 'Number of Persons of Interests (POIs) in dataset: ', poi_count
-    print 'Number of non-POIs in dataset: ', len(people_keys) - poi_count
+    # TODO: move into fill_nan_values and return (or print)
+    # Get list of NaN values and replace them
+    nan_features = {}
+    for feature in feature_keys:
+        nan_features[feature] = 0
+    for person in people_keys:
+        for feature in feature_keys:
+            if data_dict[person][feature] == 'NaN':
+                nan_features[feature] += 1
 
-# Remove outliers
-# TODO: remove outliers and fix NaN, null, etc
+    # POI with missing or no to/from email information
+    poi_missing_emails = []
+    for poi in poi_keys:
+        if (data_dict[poi]['to_messages'] == 'NaN' and
+                    data_dict[poi]['from_messages'] == 'NaN') or \
+            (data_dict[poi]['to_messages'] == 0 and
+                     data_dict[poi]['from_messages'] == 0):
+            poi_missing_emails.append(poi)
+
+    # Top 3 POI payments and stock value
+    # Top 3 non-POI payments and stock value
+
+    # May have to fix up total_payments
+    # print data_dict['BELFER ROBERT']['total_payments']
+    # Look for users with all NaN values
+    # Look for 'weird' user names
+
+    # TODO: calculate percentage allocation of classes poi/all
+
+    print 'Number of people in dataset: %d' % len(people_keys)
+    print 'Number of features for each person: %d' % len(feature_keys)
+    print 'Number of Persons of Interests (POIs) in dataset: %d out of 34 total POIs' % poi_count
+    print 'Number of non-POIs in dataset: %d' % (len(people_keys) - poi_count)
+    print nan_features
+    print 'POIs with zero or missing to/from email messages in dataset: %d' % len(poi_missing_emails)
+    print poi_missing_emails
+
+    print '### REMOVE OUTLIERS ###'
+    # Update NaN values
+    fill_nan_values(data_dict)  # having nan values messed up numeric comparisons like > < ==
+
+    # outlier at 26m in salary -> 'total
+    scatter_plot(data_dict, 'salary', 'bonus')
+    # remove outlier 'total'
+    print(data_dict['TOTAL'])
+    data_dict.pop('TOTAL')
+    scatter_plot(data_dict, 'salary', 'bonus')
+
+    # 4 outliers in scatter plot to investigate
+    # Makes over 5mil in bonus
+    bonus_bonanza = []
+    people_keys = data_dict.keys()  # Update after removal of people
+    for person in people_keys:
+        if data_dict[person]["bonus"] > 5000000 and \
+                data_dict[person]["salary"] > 1000000:
+            bonus_bonanza.append(person)
+
+    print 'Bonus Bonanza: \n', bonus_bonanza  # ['LAY KENNETH L', 'SKILLING JEFFREY K']
+
--- P5_Enron_Email_Data_Machine_Learning/final_project/poi_id.py
+++ P5_Enron_Email_Data_Machine_Learning/final_project/poi_id.py
@@ -16,18 +16,21 @@ with open("final_project_dataset.pkl", "r") as data_file:
 
 
 # Data exploration
-exploreData(data_dict)
+# exploreData(data_dict)
 
 
 # Feature engineering
-# TODO: select features, create features, test with PCA
+# TODO: select features, create features
 # features_list is a list of strings, each of which is a feature name.
 # The first feature must be "poi".
-features_list = ['poi', 'salary']
+# features_list = ['poi', 'salary', 'bonus']
+
+features_list = ['poi','salary', 'deferral_payments', 'total_payments',
+'bonus', 'restricted_stock_deferred', 'deferred_income', 'total_stock_value', 'expenses',
+'exercised_stock_options', 'long_term_incentive', 'restricted_stock']
 
 # TODO: create def to return top x features (e.g kbest)
 # TODO: create new feature(s), rerun feature section (e.g kbest)
-
 # TODO: update features_list and my_dataset
 # Store to my_dataset for easy export below.
 my_dataset = data_dict
@@ -35,6 +38,36 @@ my_dataset = data_dict
 # Extract features and labels from dataset for local testing
 data = featureFormat(my_dataset, features_list, sort_keys = True)
 labels, features = targetFeatureSplit(data)
+
+
+def test_kbest(features,labels,features_list):
+
+    from sklearn.feature_selection import SelectKBest
+
+    x = 0
+
+    kbest = SelectKBest(k=5)
+    kbestrans = kbest.fit_transform(features,labels)
+
+    for f in features_list:
+        if x > 0:
+            print(f,'KBEST',kbest.scores_[x-1])
+        x = x + 1
+
+    return kbest
+
+# Just a test
+test_kbest(features, labels, features_list)
+from sklearn.feature_selection import SelectKBest
+kbest = SelectKBest(k=5)  # retain only the top 5 features with the best score
+kbestrans = kbest.fit_transform(features, labels)  # fit and transform the data
+indices = kbest.get_support(True)  # get the indices of the features_list which were kept
+
+
+print kbestrans.shape
+print kbest.scores_[0]
+print kbestrans[0]
+print indices
 
 
 # Task 4: Try a variety of classifiers
