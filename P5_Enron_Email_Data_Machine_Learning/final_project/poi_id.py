05e1e3fc3ec922f22af7ab41846c7e582133b14a
Jason Carter <jason.ian.carter@gmail.com>
2016-06-12 01:58:43 -0400

    Added gridsearchcv

--- P5_Enron_Email_Data_Machine_Learning/final_project/poi_id.py
+++ P5_Enron_Email_Data_Machine_Learning/final_project/poi_id.py
@@ -3,12 +3,16 @@
 
 import sys
 import pickle
-import sklearn
 
 from sklearn.feature_selection import SelectKBest
 from sklearn.pipeline import Pipeline
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.grid_search import GridSearchCV
 
 sys.path.append("../tools/")
 
@@ -36,18 +40,20 @@ print '########## Feature Selection ##########'
 
 # features_list is a list of strings, each of which is a feature name.
 # The first feature must be "poi".
-features_list = ['poi', 'salary', 'deferral_payments', 'total_payments',
-                 'bonus', 'restricted_stock_deferred', 'deferred_income', 'total_stock_value', 'expenses',
-                 'exercised_stock_options', 'long_term_incentive', 'restricted_stock']
+features_list = ['poi', 'salary', 'deferral_payments', 'total_payments', 'loan_advances',
+                 'bonus', 'restricted_stock_deferred', 'deferred_income', 'total_stock_value',
+                 'expenses', 'exercised_stock_options', 'other', 'long_term_incentive',
+                 'restricted_stock', 'director_fees', 'to_messages', 'from_poi_to_this_person',
+                 'from_messages', 'from_this_person_to_poi', 'shared_receipt_with_poi']
 
 
-def featureListSelection(feats, lbls):
+def kbest_feature_list_selection(feats, lbls):
     # featureSelection.py - featureListSelection(old_feature_list, data_dict) returns new_feature_list, feature_names
 
-    # Retain only the top 5 features with the best score, fit and transform data
+    # Retain only the top x features with the best score then fit and transform data
     # Get the indices of the feat_list which were kept
-    kbest = SelectKBest(k=5)
-    ktransform = kbest.fit_transform(feats, lbls)
+    kbest = SelectKBest(k=10)
+    kbfeatures = kbest.fit_transform(feats, lbls)
     indices = kbest.get_support(True)
 
     # feature_list must start with labels
@@ -58,47 +64,84 @@ def featureListSelection(feats, lbls):
         feats_list.append(features_list[index])
         print '%s: %f' % (features_list[index], kbest.scores_[index])
 
-    return feats_list, ktransform, kbest
+    return feats_list, kbfeatures, kbest
 
 
-def buildClassifierPipeline(classifier_type):
+def build_classifier_pipeline(classifier_type):
     # featureSelection.py - classifierTest(feature_list, clf,...) return clf, etc..
 
-    # TODO: GridSearchCV
+    # build pipeline
+    classifier = set_classifier(classifier_type)
+    pipeline = Pipeline(steps=[('feature_selection', kb), (classifier_type, classifier)])
 
-    # build switch statement
-    classifier = setClassifier(classifier_type)
-    return Pipeline(steps=[('kBest', kb), (classifier_type, classifier)])
+    # Get train test sets
+    features_train, features_test, labels_train, labels_test = \
+        train_test_split(kb_features, labels, test_size=0.5, random_state=42)
 
-def runCrossValScore(classifier_type):
+    # Set parameters for random forest
+    parameters = []
+    if classifier_type == 'random_forest':
+        parameters = dict(feature_selection__k=[5, 10, 'all'],
+                          random_forest__n_estimators=[25, 50, 75, 100],
+                          random_forest__min_samples_split=[2, 3, 4, 5],
+                          random_forest__bootstrap=[True, False],
+                          random_forest__criterion=['gini', 'entropy'])
+
+    cv = GridSearchCV(pipeline, param_grid=parameters)
+
+    cv.fit(features_train, labels_train)
+    predictions = cv.predict(features_test)
+    report = classification_report(labels_test, predictions)
+
+    return report, cv
+
+
+def run_cross_val_score(classifier_type):
     import sklearn.ensemble
 
-    clf = setClassifier(classifier_type)
-    score = sklearn.cross_validation.cross_val_score(clf, kb_transform, labels)
-    print(score)
+    clf = set_classifier(classifier_type)
+    score = sklearn.cross_validation.cross_val_score(clf, kb_features, labels)
+    print score
 
 
-def setClassifier(x):
+def set_classifier(x):
     # switch statement Python replacement - http://stackoverflow.com/a/103081
     return {
-        'random_forest': RandomForestClassifier(n_estimators=1, bootstrap=False),
-        'decision_tree': DecisionTreeClassifier()
+        'random_forest': RandomForestClassifier(),
+        'decision_tree': DecisionTreeClassifier(),
+        'logistic_reg': LogisticRegression(),
+        'gaussian_nb': GaussianNB()
     }.get(x)
 
+
+# Pre-processing removal of columns for model
+# Removed 'email_address' to prepare for ML algorithm
 
 # Extract features and labels from dataset for local testing
 data = featureFormat(my_dataset, features_list, sort_keys=True)
 labels, features = targetFeatureSplit(data)
 
 
+# Having so many features invites problems with overfitting,
+# and don’t need all 3 thousand features to capture the patterns in my dataset.
+# This is a perfect use case for feature selection,
+# which is supported in scikit-learn by e.g. SelectKBest(),
+# which will do univariate feature selection to get the k features
+# (where k is a number which I have to tell the algorithm).
+
+
 # Select the best features and return new features with transform
-f_list, kb_transform, kb = featureListSelection(features, labels)
-features_list = f_list
+kb_features_list, kb_features, kb = kbest_feature_list_selection(features, labels)
+features_list = kb_features_list
 
 
 # Cross_val_score testing to pick and test classifier then use picked clf for pipeline
-runCrossValScore('decision_tree')
-runCrossValScore('random_forest')
+# Categorical models not linear...
+run_cross_val_score('decision_tree')  # [ 0.83673469  0.79166667  0.83333333]
+run_cross_val_score('random_forest')  # [ 0.87755102  0.875       0.875     ]
+run_cross_val_score('logistic_reg')   # [ 0.75510204  0.85416667  0.85416667]
+run_cross_val_score('gaussian_nb')    # [ 0.30612245  0.27083333  0.27083333]
+
 
 # Test classifiers
 print '########## Test and Tune Classifiers ##########'
@@ -108,11 +151,18 @@ print '########## Test and Tune Classifiers ##########'
 # function. Because of the small size of the dataset, the script uses
 # stratified shuffle split cross validation. For more info:
 # http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
-#clf = buildClassifierPipeline('random_forest')
 
-# TODO: test classifier pipeline before using test.py - use the gridsearchcv from katie
+rpt, cross_val = build_classifier_pipeline('random_forest')
+print cross_val.best_params_
+print rpt
 
-#test_classifier(clf, my_dataset, features_list, folds=100)
+# {'feature_selection__k': 10, 'random_forest__n_estimators': 50, 'random_forest__min_samples_split': 4}
+
+# See test results
+# selector = SelectKBest(k=10)
+# classifier = RandomForestClassifier(n_estimators=50, min_samples_split=4)
+# clf = Pipeline(steps=[('feature_selection', selector), ('random_forest', classifier)])
+# test_classifier(clf, my_dataset, features_list)
 
 
 # Task 6: Dump your classifier, dataset, and features_list so anyone can
@@ -126,3 +176,4 @@ print '########## Test and Tune Classifiers ##########'
 # https://civisanalytics.com/blog/data-science/2016/01/06/workflows-python-using-pipeline-gridsearchcv-for-compact-code/
 # http://scikit-learn.org/stable/modules/generated/sklearn.feature_selection.SelectKBest.html
 # http://scikit-learn.org/stable/modules/pipeline.html
+# https://civisanalytics.com/blog/data-science/2015/12/17/workflows-in-python-getting-data-ready-to-build-models/
