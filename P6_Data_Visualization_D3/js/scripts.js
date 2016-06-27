fb358f374dfb325a52449042ffdd6dd3be9cce2a
Jason Carter <jason.ca@myplanet.com>
2016-06-27 16:52:45 -0400

    Initial testing of D3

--- P6_Data_Visualization_D3/js/scripts.js
+++ P6_Data_Visualization_D3/js/scripts.js
@@ -1,1 +1,12 @@
-hello world
+var data = [4, 8, 15, 16, 23, 42];
+
+var x = d3.scale.linear()
+    .domain([0, d3.max(data)])
+    .range([0, 420]);
+
+d3.select(".chart")
+  .selectAll("div")
+    .data(data)
+  .enter().append("div")
+    .style("width", function(d) { return x(d) + "px"; })
+    .text(function(d) { return d; });
