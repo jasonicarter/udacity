b3ef19534516bfd3e1e000756f0bbc1f0c94c96d
Jason Carter <jason.ca@myplanet.com>
2016-07-06 19:36:01 -0400

    Added example work

--- P6_Data_Visualization_D3/js/scripts.js
+++ P6_Data_Visualization_D3/js/scripts.js
@@ -1,12 +1,8 @@
-var data = [4, 8, 15, 16, 23, 42];
+//Width and height
+var w = 500;
+var h = 50;
 
-var x = d3.scale.linear()
-    .domain([0, d3.max(data)])
-    .range([0, 420]);
-
-d3.select(".chart")
-  .selectAll("div")
-    .data(data)
-  .enter().append("div")
-    .style("width", function(d) { return x(d) + "px"; })
-    .text(function(d) { return d; });
+var svg = d3.select("body")
+            .append("svg")
+            .attr("width", w) 
+            .attr("height", h);
