09fa49ffb0ffca81735a02fbc6bc5821f8adf742
Jason Carter <jason.ca@myplanet.com>
2016-07-11 15:14:44 -0400

    Added comm housing percentage as hist

--- P6_Data_Visualization_D3/js/scripts.js
+++ P6_Data_Visualization_D3/js/scripts.js
@@ -1,17 +1,13 @@
 // modified from http://neuralengr.com/asifr/journals/
-var margin = {top: 20, right: 200, bottom: 0, left: 20}, // TODO: don't really need margin.right
+var margin = {top: 20, right: 400, bottom: 0, left: 20}, // TODO: don't really need margin.right
 	width = 400,
 	height = 650; // TODO: need to update for ~50 neighbourhoods
 
+var maxBarWidth = 200,
+    barHeight = 20;
+
 // http://bl.ocks.org/aaizemberg/78bd3dade9593896a59d
 var c10 = d3.scale.category10();
-
-var svg = d3.select("body").append("svg")
-	.attr("width", width + margin.left + margin.right)
-	.attr("height", height + margin.top + margin.bottom)
-	.style("margin-left", margin.left + "px")
-	.append("g") // D3 group element
-	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 // TODO: pull this from dataset
 var x = d3.scale.ordinal()
@@ -24,6 +20,21 @@ var xAxis = d3.svg.axis()
     .scale(x)
     .orient("top");
 
+var xBarScale = d3.scale.linear()
+    .domain([0, 1]) //d3.max(data, function(d) { return d.comm_housing_pop_ratio; })]);
+    .range([0, 10]);
+
+// http://bl.ocks.org/mstanaland/6106487
+var formatDecimal = d3.format(".2f")
+
+// TODO: update with with bar width
+var svg = d3.select("body").append("svg")
+	.attr("width", width + margin.left + margin.right)
+	.attr("height", height + margin.top + margin.bottom)
+	.style("margin-left", margin.left + "px")
+	.append("g") // D3 group element
+	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
 svg.append("g")
     .attr("class", "x axis")
     .attr("transform", "translate(" + 165 + "," + 0 + ")")
@@ -35,25 +46,24 @@ svg.append("g")
     .attr("transform", "rotate(-45)")
     .style("text-anchor", "start");
 
-
 // Load data and create svg elements
-d3.json("data/sample_data.json", function(data) {
-  
-//    TODO: look into using a scale and xAxis
+d3.json("data/data_formatted.json", function(data) {
+    
+    // Apply svg elements for each individual record / neighbourhood
 	for (var j = 0; j < data.length; j++) {
 		var g = svg.append("g")
             .attr("class","neighbourhood")
             .attr("transform", "translate(" + 0 + "," + j*5 + ")");
 
 		var circles = g.selectAll("circle")
-			.data(data[j]['crime_types'])
+                .data(data[j]['crime_types'])
 			.enter()
-			.append("circle");
+                .append("circle");
 
 		var text = g.selectAll("text")
-			.data(data[j]['crime_types'])
+                .data(data[j]['crime_types'])
 			.enter()
-			.append("text");
+                .append("text");
       
         var rScale = d3.scale.sqrt() // d3.scale.linear()
             .domain([1, 800 ]) // .domain([0, d3.max(data[j]['crime_types'], function(d) { return d3.values(d)[0]; }) ])
@@ -80,10 +90,24 @@ d3.json("data/sample_data.json", function(data) {
 			.attr("x", 140) // Setting text-anchor to end means x must be x-(text.length) where text.length is max px of label
 			.attr("class", "label")
 			.text(data[j]['name'])
-			.style("fill", "black")
             .style("text-anchor", "end")
 			.on("mouseover", mouseover)
 			.on("mouseout", mouseout);
+        
+        g.append("rect")
+            .attr("width", xBarScale(data[j]["comm_housing_pop_ratio"])*100)
+            .attr("height", barHeight)
+            .attr("class", "housing")
+            .attr("y", j*20+25/2) // center rect on each neighbourhood
+            .attr("transform", "translate(" + 600 + "," + 0 + ")") // TODO: get this working with variables
+            .style("fill", c10(0));
+            
+        g.append("text")
+            .attr("x", xBarScale(data[j]["comm_housing_pop_ratio"])*100)
+            .attr("y", j*20+25/2)
+            .attr("dy", "1.35em") //vertical align middle
+            .attr("transform", "translate(" + 610 + "," + 0 + ")") // TODO: use exiting or put in variables at top
+            .text(formatDecimal(data[j]["comm_housing_pop_ratio"]*100) + "%");
       
 	};
 
