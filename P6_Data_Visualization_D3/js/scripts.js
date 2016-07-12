f6ce5a31aab70a357304c82a1b5cbe6d927c92db
Jason Carter <jason.ca@myplanet.com>
2016-07-12 14:59:52 -0400

    Added a reference link

--- P6_Data_Visualization_D3/js/scripts.js
+++ P6_Data_Visualization_D3/js/scripts.js
@@ -7,7 +7,7 @@ https://blog.repustate.com/twitter-postgis-d3-rob-ford-fun-with-data-visualizati
 http://www1.toronto.ca/wps/portal/contentonly?vgnextoid=04b489fe9c18b210VgnVCM1000003dd60f89RCRD&vgnextchannel=75d6e03bb8d1e310VgnVCM10000071d60f89RCRD
 http://geojson.org
 http://www1.toronto.ca/wps/portal/contentonly?vgnextoid=b1533f0aacaaa210VgnVCM1000006cd60f89RCRD&vgnextchannel=1a66e03bb8d1e310VgnVCM10000071d60f89RCRD
-
+https://suffenus.wordpress.com/2014/01/07/making-interactive-maps-with-d3-for-total-beginners/
 */
 
 
