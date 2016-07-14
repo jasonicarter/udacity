94d1b7d3cc6e1c846e8a0c408d764b9cf086d832
Jason Carter <jason.ian.carter@gmail.com>
2016-07-14 19:33:40 -0400

    Added interactive map

--- P6_Data_Visualization_D3/js/scripts.js
+++ P6_Data_Visualization_D3/js/scripts.js
@@ -43,15 +43,37 @@ var xBarScale = d3.scale.linear()
 // http://bl.ocks.org/mstanaland/6106487
 var formatDecimal = d3.format(".2f")
 
+// http://bl.ocks.org/michellechandra/0b2ce4923dc9b5809922
+var mapWidth = 350,
+    mapHeight = 350;
+
+var projection = d3.geo.albers();
+
+var path = d3.geo.path()
+    .projection(projection);
+
 // TODO: update with with bar width
 var svg = d3.select("body").append("svg")
-	.attr("width", width + margin.left + margin.right)
-	.attr("height", height + margin.top + margin.bottom)
+	.attr("width", width + margin.left + margin.right + mapWidth)
+	.attr("height", height + margin.top + margin.bottom + mapHeight)
 	.style("margin-left", margin.left + "px")
-	.append("g") // D3 group element
+
+var chartGroup = svg.append("g") // D3 group element
+    .attr("class", "chartGroup")
 	.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-svg.append("g")
+var mapGroup = svg.append("g")
+    .attr("class", "mapGroup")
+    .attr("width", mapWidth)
+    .attr("height", mapHeight);
+
+var mapLabel = mapGroup.append("text")
+    .attr("y", 20)
+    .attr("x", 0)
+    .attr("class", "map_neighbourhood_name")
+
+// xAxis group
+chartGroup.append("g")
     .attr("class", "x axis")
     .attr("transform", "translate(" + 165 + "," + 0 + ")")
     .call(xAxis)
@@ -62,80 +84,144 @@ svg.append("g")
     .attr("transform", "rotate(-45)")
     .style("text-anchor", "start");
 
-// Load data and create svg elements
-d3.json("data/sample_data.json", function(data) {
-    
-    // Apply svg elements for each individual record / neighbourhood
-	for (var j = 0; j < data.length; j++) {
-		var g = svg.append("g")
-            .attr("class","neighbourhood")
-            .attr("transform", "translate(" + 0 + "," + j*5 + ")");
+// load neighbourhood crime data
+d3.json("data/sample_data.json", function(error, data) {
+  if (error) throw error;
 
-		var circles = g.selectAll("circle")
-                .data(data[j]['crime_types'])
-			.enter()
-                .append("circle");
+  // Apply svg elements for each individual record / neighbourhood
+  for (var j = 0; j < data.length; j++) {
+      var g = chartGroup.append("g")
+          .attr("class","neighbourhood")
+          .attr("transform", "translate(" + 0 + "," + j*5 + ")");
 
-		var text = g.selectAll("text")
-                .data(data[j]['crime_types'])
-			.enter()
-                .append("text");
-      
-        var rScale = d3.scale.sqrt() // d3.scale.linear()
-            .domain([1, 800 ]) // .domain([0, d3.max(data[j]['crime_types'], function(d) { return d3.values(d)[0]; }) ])
-            .range([1, 10])
-            // .clamp(true);
-        
-        circles
-            .attr("cx", function(d, i) { return i*35+175; })
-            .attr("cy", j*20+20)
-            .attr("r", function(d) { return rScale(d3.values(d)[0]); })
-            .style("fill", function(d,i) { return c10(i); });
-      
-		text
-			.attr("y", j*20+25)
-			.attr("x",function(d, i) { return i*35+175; })
-			.attr("class", "value")
-			.text(function(d){ return d3.values(d)[0]; }) // each d = {key:value} of crime_types
-			.style("fill", function(d,i) { return c10(i); })
-            .style("text-anchor", "middle")
-			.style("display", "none");
+      var circles = g.selectAll("circle")
+              .data(data[j]['crime_types'])
+          .enter()
+              .append("circle");
 
-		g.append("text")
-			.attr("y", j*20+25)
-			.attr("x", 140) // Setting text-anchor to end means x must be x-(text.length) where text.length is max px of label
-			.attr("class", "label")
-			.text(data[j]['name'])
-            .style("text-anchor", "end")
-			.on("mouseover", mouseover)
-			.on("mouseout", mouseout);
-        
-        g.append("rect")
-            .attr("width", xBarScale(data[j]["comm_housing_pop_ratio"])*100)
-            .attr("height", barHeight)
-            .attr("class", "housing")
-            .attr("y", j*20+25/2) // center rect on each neighbourhood
-            .attr("transform", "translate(" + 600 + "," + 0 + ")") // TODO: get this working with variables
-            .style("fill", c10(0));
-            
-        g.append("text")
-            .attr("x", xBarScale(data[j]["comm_housing_pop_ratio"])*100)
-            .attr("y", j*20+25/2)
-            .attr("dy", "1.35em") //vertical align middle
-            .attr("transform", "translate(" + 610 + "," + 0 + ")") // TODO: use exiting or put in variables at top
-            .text(formatDecimal(data[j]["comm_housing_pop_ratio"]*100) + "%");
-      
-	};
-    
-	function mouseover(p) {
-		var g = d3.select(this).node().parentNode;
-		d3.select(g).selectAll("circle").style("display","none");
-		d3.select(g).selectAll("text.value").style("display","block");
-	}
+      var text = g.selectAll("text")
+              .data(data[j]['crime_types'])
+          .enter()
+              .append("text");
 
-	function mouseout(p) {
-		var g = d3.select(this).node().parentNode;
-		d3.select(g).selectAll("circle").style("display","block");
-		d3.select(g).selectAll("text.value").style("display","none");
-	}
+      var rScale = d3.scale.sqrt() // d3.scale.linear()
+          .domain([1, 800 ]) // .domain([0, d3.max(data[j]['crime_types'], function(d) { return d3.values(d)[0]; }) ])
+          .range([1, 10])
+          // .clamp(true);
+
+      circles
+          .attr("cx", function(d, i) { return i*35+175; })
+          .attr("cy", j*20+20)
+          .attr("r", function(d) { return rScale(d3.values(d)[0]); })
+          .style("fill", function(d,i) { return c10(i); });
+
+      text
+          .attr("y", j*20+25)
+          .attr("x",function(d, i) { return i*35+175; })
+          .attr("class", "value")
+          .text(function(d){ return d3.values(d)[0]; }) // each d = {key:value} of crime_types
+          .style("fill", function(d,i) { return c10(i); })
+          .style("text-anchor", "middle")
+          .style("display", "none");
+
+      g.append("text")
+          .attr("y", j*20+25)
+          .attr("x", 140) // Setting text-anchor to end means x must be x-(text.length) where text.length is max px of label
+          .attr("class", "label")
+          .text(data[j]['name'])
+          .style("text-anchor", "end")
+          .on("mouseover", mouseover)
+          .on("mouseout", mouseout);
+
+      g.append("rect")
+          .attr("width", xBarScale(data[j]["comm_housing_pop_ratio"])*100)
+          .attr("height", barHeight)
+          .attr("class", "housing")
+          .attr("y", j*20+25/2) // center rect on each neighbourhood
+          .attr("transform", "translate(" + 600 + "," + 0 + ")") // TODO: get this working with variables
+          .style("fill", c10(0));
+
+      g.append("text")
+          .attr("x", xBarScale(data[j]["comm_housing_pop_ratio"])*100)
+          .attr("y", j*20+25/2)
+          .attr("dy", "1.35em") //vertical align middle
+          .attr("transform", "translate(" + 610 + "," + 0 + ")") // TODO: use exiting or put in variables at top
+          .text(formatDecimal(data[j]["comm_housing_pop_ratio"]*100) + "%");
+
+  };
+
+  function mouseover(p) {
+      var g = d3.select(this).node().parentNode;
+      d3.select(g).selectAll("circle").style("display","none");
+      d3.select(g).selectAll("text.value").style("display","block");
+  }
+
+  function mouseout(p) {
+      var g = d3.select(this).node().parentNode;
+      d3.select(g).selectAll("circle").style("display","block");
+      d3.select(g).selectAll("text.value").style("display","none");
+  }
+  
+});
+
+// load neighbourhood map data
+d3.json("data/toronto_topo.json", function(error, toronto) {
+  if (error) throw error;
+
+  var neighbourhoods = topojson.feature(toronto, toronto.objects.toronto); //,
+//                   neighbourhood = neighbourhoods.features.filter(function(d) { return d.properties.id == 97; })[0];
+
+  // set default projection values 
+  projection
+      .scale(1)
+      .translate([0, 0]);
+
+  // creates bounding box and helps with projection and scaling
+  var b = path.bounds(neighbourhoods),
+      s = .95 / Math.max((b[1][0] - b[0][0]) / mapWidth, (b[1][1] - b[0][1]) / mapHeight),
+      t = [(mapWidth - s * (b[1][0] + b[0][0])) / 2, (mapHeight - s * (b[1][1] + b[0][1])) / 2];
+
+  // set project with bounding box data
+  projection
+      .scale(s)
+      .translate(t);
+
+  // get individual neighbourhoods
+  mapGroup.selectAll("path")
+        .data(neighbourhoods.features)
+      .enter().append("path")
+        .attr("class", "map_neighbourhood")
+        .attr("d", path)
+        .on("mouseover", mouseover) 
+        .on("mouseout", mouseout)
+        .on("click", clicked)
+
+  mapGroup.append("path")
+      .datum(topojson.mesh(toronto, toronto.objects.toronto, function(a, b) { return a !== b; }))
+      .attr("class", "map_mesh")
+      .attr("d", path);
+
+//              svg.append("path")
+//                  .datum(neighbourhood)
+//                  .attr("class", "map_outline")
+//                  .attr("d", path)
+  
+  function mouseover(d) {     
+    mapLabel.text(d.properties.name.slice(0,-5)) // remove suffix id from name
+  }
+
+  function mouseout(d) {     
+    mapLabel.text("")  
+  }
+
+  function clicked(d) {
+    console.log(d.properties.id, d.properties.name)
+    /*TODO: 
+      set selected to true
+      add d.id to list or dictionary
+      if selected == true, remove d.id from list
+      update text showing list
+    */
+  }
+  
 });
